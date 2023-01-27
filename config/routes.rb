5da8d535410ae0ee4f237df7edb6bfb3b861071b
mateovieyra <mateovieyra8@gmail.com>
2023-01-27 11:39:55 -0300

    Create about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
+
+  get '/about', to: 'pages#about'
 end
