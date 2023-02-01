d8c4afc4c2d5d6014ab340f03aec18ebdbbddd47
mateovieyra <mateovieyra8@gmail.com>
2023-02-01 09:22:45 -0300

    Style form New and Edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = 'Article was created successfully'
       redirect_to @article
     else
-      render 'new'
+      render 'new', status: 422
     end
   end
 
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = 'Article was updated successfully'
       redirect_to @article
     else
-      render 'edit'
+      render 'edit', status: 422
     end
   end
 
