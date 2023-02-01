3a76bb98ea886aec250aaf030ea28c38b51859f9
mateovieyra <mateovieyra8@gmail.com>
2023-02-01 15:25:49 -0300

    Create users profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 80})
+    email_adress = user.email.downcase 
+    hash = Digest::MD5.hexdigest(email_adress)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded mx-auto d-block shadow")
+  end
 end
