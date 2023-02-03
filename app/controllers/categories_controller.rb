9015fe5169401f05c268751c120e058f4e4577b3
mateovieyra <mateovieyra8@gmail.com>
2023-02-03 09:35:02 -0300

    Create categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < ApplicationController
+  
+  before_action :set_category, only: [:show]
+
+  def new 
+    @category = Category.new
+  end
+
+  def show 
+    
+  end
+
+  def index 
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category  
+    else
+      render 'new', status: 422
+    end
+  end
+
+  private 
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params 
+    params.require(:category).permit(:name)
+  end
+
+end
